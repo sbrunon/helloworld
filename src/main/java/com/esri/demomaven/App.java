01012fc78f62d6aa38d1267958d5d4d69490438e
kdazzazi <kdazzazi@esrifrance.fr>
2019-06-19 14:19:01 +0200

    Initial commit

---
+++ src/main/java/com/esri/demomaven/App.java
@@ -0,0 +1,13 @@
+package com.esri.demomaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
